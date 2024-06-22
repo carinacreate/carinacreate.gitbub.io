7b81a1fb8aa71eae4f01cf3162922654c18503af
zhangwenqing <153161238+carinacreate@users.noreply.github.com>
2024-06-22 20:37:51 +0800

    Update photographic.js

--- js/photographic.js
+++ js/photographic.js
@@ -1,30 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
     const closeBtn = document.getElementsByClassName('close')[0];
     const images = document.querySelectorAll('.image-section img');
-
-    // 显示模态框并放大图片
-    images.forEach(img => {
-        img.addEventListener('click', function() {
-            modal.style.display = 'flex';
-            modalImg.src = this.src;
-            document.body.style.overflow = 'hidden'; // 禁止背景滚动
-        });
-    });
-
-    // 关闭模态框
-    closeBtn.addEventListener('click', function() {
-        modal.style.display = 'none';
-        document.body.style.overflow = 'auto'; // 恢复背景滚动
-    });
-
-    window.addEventListener('click', function(event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-            document.body.style.overflow = 'auto'; // 恢复背景滚动
-        }
-    });
 
     // 滚动时调整图片透明度和位置
     window.addEventListener('scroll', function() {
